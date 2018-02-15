2ed8553c0b0fb5d09a2e8d3048cfecbdb262bfa7
adaitsDAW <jorge.baron@adaits.es>
2018-02-15 20:12:04 +0100

    Clase Minion

--- src/com/adaits/Minion.java
+++ src/com/adaits/Minion.java
@@ -5,6 +5,13 @@ public class Minion {
     private String nombre;
     private int crimenes;
 
+    //Crear propiedad para gestionar palabra favorita
+    //Crear propiedad para los numeros de ojos
+    //Crear propiedad de si lleva gafas o no
+    //Crear propiedad de habilidades
+    //Crear Clase Jefe y asociarla a un minion, tener en cuenta que un jefe puede tener varios minions
+    //Pero los minions solo obedecen a un jefe
+
     public Minion(String nombre, int crimenes) {
         this.nombre = nombre;
         this.crimenes = crimenes;
@@ -35,5 +42,15 @@ public class Minion {
 
         return esMayor;
     }
+
+    //Crear una funcion que obtengan todos los minions con mas de un ojo y sin gafas
+
+
+    //Crear una función que obtenga todos los minions con más de una habilidad
+
+
+    //Crear una función que obtenga un minion con la misma palabra favorita que tu
+
+    //Crear una función que te devuelva el primer minion que se unio a un jefe
 
 }
